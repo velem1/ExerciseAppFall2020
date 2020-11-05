aae5bacbd937267cfab0f1c999cc43ee50b7700b
Melissa Vele <melissavele1@gmail.com>
2020-11-05 15:52:10 -0500

    Database structure update

--- server/sql/CreateDatabase.sql
+++ server/sql/CreateDatabase.sql
@@ -18,8 +18,8 @@ CREATE TABLE IF NOT EXISTS `Types` (
   `Name` VARCHAR(45) NOT NULL,
   `Type_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `Name_UNIQUE` (`Name` ASC) VISIBLE,
-  INDEX `fk_Types_Types1_idx` (`Type_id` ASC) VISIBLE,
+  UNIQUE INDEX `Name_UNIQUE` (`Name` ASC),
+  INDEX `fk_Types_Types1_idx` (`Type_id` ASC),
   CONSTRAINT `fk_Types_types1`
     FOREIGN KEY (`Type_id`)
     REFERENCES `Types` (`id`)
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `Users` (
   `Password` VARCHAR(45) NULL,
   `User_Types` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_Users_Types1_idx` (`User_Types` ASC) VISIBLE,
+  INDEX `fk_Users_Types1_idx` (`User_Types` ASC),
   CONSTRAINT `fk_Users_Types1`
     FOREIGN KEY (`User_Types`)
     REFERENCES `Types` (`id`)
@@ -61,8 +61,8 @@ CREATE TABLE IF NOT EXISTS `ContactMethods` (
   `IsPrimary` BIT NOT NULL DEFAULT 0,
   `User_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_ContacMethods_Users_idx` (`User_id` ASC) VISIBLE,
-  INDEX `fk_ContactMethod_Type_idx` (`Type` ASC) VISIBLE,
+  INDEX `fk_ContacMethods_Users_idx` (`User_id` ASC),
+  INDEX `fk_ContactMethod_Type_idx` (`Type` ASC),
   CONSTRAINT `fk_ContactMethods_Users`
     FOREIGN KEY (`User_id`)
     REFERENCES `Users` (`id`)
@@ -91,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `ExercisePost` (
   `Privacy_Setting` INT NULL COMMENT '0 - hidden\n1 - only me\n2 - only friends\n3 - public',
   `Owner_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_ExercisePost_Users1_idx` (`Owner_id` ASC) VISIBLE,
+  INDEX `fk_ExercisePost_Users1_idx` (`Owner_id` ASC),
   CONSTRAINT `fk_Posts_Users1`
     FOREIGN KEY (`Owner_id`)
     REFERENCES `Users` (`id`)
@@ -111,8 +111,8 @@ CREATE TABLE IF NOT EXISTS `Comments` (
   `ExercisePost_id` INT NOT NULL,
   `Owner_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_Comments_ExercisePosts1_idx` (`ExercisePost_id` ASC) VISIBLE,
-  INDEX `fk_Comments_Users1_idx` (`Owner_id` ASC) VISIBLE,
+  INDEX `fk_Comments_ExercisePosts1_idx` (`ExercisePost_id` ASC),
+  INDEX `fk_Comments_Users1_idx` (`Owner_id` ASC),
   CONSTRAINT `fk_Comments_Posts1`
     FOREIGN KEY (`ExercisePost_id`)
     REFERENCES `ExercisePost` (`id`)
@@ -164,15 +164,15 @@ INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES 
 INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, 'Now()', DEFAULT, 'Email', 4);
 INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (7, 'Now()', DEFAULT, 'Yoga Types', 1);
 INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (8, 'Now()', DEFAULT, 'Yoga Difficulty Types', 1);
-INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (, 'Now()', DEFAULT, 'Vinyasa', 7);
-INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (, 'Now()', DEFAULT, 'Kundalini', 7);
-INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (, 'Now()', DEFAULT, 'Restorative', 7);
-INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (, 'Now()', DEFAULT, 'Power', 7);
-INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (, 'Now()', DEFAULT, 'General', 7);
-INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (, 'Now()', DEFAULT, 'Beginner', 8);
-INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, DEFAULT, DEFAULT, 'Beginner-Intermediate', 8);
-INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, DEFAULT, DEFAULT, 'Intermediate', 8);
-INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, DEFAULT, DEFAULT, 'Intermediate-Advanced', 8);
-INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, DEFAULT, DEFAULT, 'Advanced', 8);
+INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, 'Now()', DEFAULT, 'Vinyasa', 7);
+INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, 'Now()', DEFAULT, 'Kundalini', 7);
+INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, 'Now()', DEFAULT, 'Restorative', 7);
+INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, 'Now()', DEFAULT, 'Power', 7);
+INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, 'Now()', DEFAULT, 'General', 7);
+INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, 'Now()', DEFAULT, 'Beginner', 8);
+INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, 'Now()', DEFAULT, 'Beginner-Intermediate', 8);
+INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, 'Now()', DEFAULT, 'Intermediate', 8);
+INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, 'Now()', DEFAULT, 'Intermediate-Advanced', 8);
+INSERT INTO `Types` (`id`, `created_at`, `update_at`, `Name`, `Type_id`) VALUES (DEFAULT, 'Now()', DEFAULT, 'Advanced', 8);
 
 COMMIT;
