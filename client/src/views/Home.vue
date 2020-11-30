e03b349c9d55c72a4aebe192330959e9fe7b5933
Melissa Vele <melissavele1@gmail.com>
2020-11-29 23:56:02 -0500

    front end updates

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,20 +1,23 @@
 <template>
   <div class="section">
-    <h1 class="title">Welcome</h1>
+    <h1 class="title">Welcome to The Yoga Tracker</h1>
     <br>
-      <h1 class="subtitle">Click the button below to add to your yoga log.</h1>
-      <router-link to="/addexercise" >
-        <a class="bd-tw-button button" target="_blank">
+
+    <!-- <div class="image is-5by3">
+      <img src="../assets/balance.png" alt="Calming Ocean and Rocks">
+    </div> -->
+
+    <h1 class="subtitle">Click the <strong>button</strong> below to add to your yoga log.</h1>
+    <router-link to="/SubmitExercise">
+      <a class="bd-tw-button button" target="_blank">
         Add Exercise
-        </a>
-      </router-link> 
-    </div>
+      </a>
+    </router-link> 
+  </div>
   
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: "Home",
   components: {
---
+++ client/src/views/SubmitExercise.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="section">
+  <div class="container">
+    <AddExercise/>    
+  </div>
+    <PersonalLog />
+  </div>
+</template>
+
+<script>
+import AddExercise from "../components/AddExercise"
+import PersonalLog from "../components/PersonalLog"
+export default {
+  name: "SubmitExercise",
+  components: {
+    AddExercise,
+    PersonalLog
+  },
+};
+</script>
